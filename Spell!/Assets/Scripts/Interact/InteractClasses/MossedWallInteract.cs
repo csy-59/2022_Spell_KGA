4dd673b32f6c1b2206a9d9558931f4878d2bf190
csy-59 <csy010509@naver.com>
2022-08-24 15:23:36 +0900

    [Feat] 이끼 낀 벽 생성 완료

---
+++ Spell!/Assets/Scripts/Interact/InteractClasses/MossedWallInteract.cs
@@ -0,0 +1,65 @@
+using InteractAsset;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MossedWallInteract : InteractiveObject
+{
+    [Header("Moss Interact")]
+    [SerializeField] private int mossCount;
+    [SerializeField] private GameObject moss;
+    
+    [SerializeField] private Vector3 minMossPosition;
+    [SerializeField] private Vector3 maxMossPosition;
+
+    [SerializeField] private float minScale = 0.06f;
+    [SerializeField] private float maxScale = 0.11f;
+
+    [SerializeField] private float mossPickForce = 2f;
+    
+    private Stack<GameObject> mossStack = new Stack<GameObject>();
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        bool[] isPushed = { false };
+        for(int i = 0; i < mossCount; ++i)
+        {
+            float x = Random.Range(minMossPosition.x, maxMossPosition.x);
+            float y = Random.Range(minMossPosition.y, maxMossPosition.y);
+            Vector3 newPosition = new Vector3(x, y, maxMossPosition.z);
+
+            float scale = Random.Range(minScale, maxScale);
+
+            GameObject newMoss = Instantiate(moss, transform);
+            newMoss.transform.Translate(newPosition);
+            newMoss.transform.Rotate(90f, 0f, 0f);
+            newMoss.transform.localScale = new Vector3(scale, scale, scale);
+
+            mossStack.Push(newMoss);
+        }
+    }
+
+    public override void Interact(ItemList item, EffectList effect)
+    {
+        if(mossStack.Count <= 0)
+        {
+            return;
+        }
+
+        UIManager.Instance.SetInfoTextBar("It's slimy...");
+
+        GameObject pickedMoss = mossStack.Pop();
+        pickedMoss.transform.parent = null;
+        Rigidbody mossRigidbody = pickedMoss.AddComponent<Rigidbody>();
+        pickedMoss.GetComponent<BoxCollider>().enabled = true;
+        mossRigidbody.AddForce(Vector3.forward * mossPickForce, ForceMode.Impulse);
+
+        if(mossStack.Count <= 0)
+        {
+            gameObject.name = "Cleaned Wall";
+        }
+    }
+
+}
