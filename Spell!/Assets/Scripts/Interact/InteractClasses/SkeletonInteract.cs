7269b9b6f53c51be05b4884e308d5d7eb9184aac
csy-59 <csy010509@naver.com>
2022-08-24 11:38:05 +0900

    [Fix] 스켈래톤에서 뼈 나올 수 있도록 조정

--- Spell!/Assets/Scripts/Interact/InteractClasses/SkeletonInteract.cs
+++ Spell!/Assets/Scripts/Interact/InteractClasses/SkeletonInteract.cs
@@ -5,29 +5,62 @@ using UnityEngine;
 
 public class SkeletonInteract : InteractiveObject
 {
+    [Header ("\nSkeleton Item Interaction")]
     [SerializeField] private GameObject item;
-    private bool isItemGiven = false;
-
     [SerializeField] private string getItemLine = "SomeThing dropped";
     [SerializeField] private string afterItemLine = "I think it's better not to touch him...";
+    private bool isItemGiven = false;
+
+    [Header ("\nSkeleton bone Interaction")]
+    [SerializeField] private GameObject bone;
+    [SerializeField] private Transform bonePosition;
+    [SerializeField] private string getBoneLine = "He's already dead, right?";
+    [SerializeField] private string afterBoneLine = "Sorry for that...";
+    private bool isBoneGiven = false;
 
     protected override void Awake()
     {
         base.Awake();
+
+        necessaryEffect = new EffectList[]
+        {
+            EffectList.DontCare,
+            EffectList.Power
+        };
+        necessaryItem = new ItemList[]
+        {
+            ItemList.DontCare,
+            ItemList.DontCare
+        };
 
         item.SetActive(false);
     }
 
     public override void Interact(ItemList item, EffectList effect)
     {
-        if(isItemGiven)
+        if(base.InteractPreAssert(item, effect, 1))
         {
-            UIManager.Instance.SetInfoTextBar(afterItemLine);
-            return;
-        }
+            if(isBoneGiven)
+            {
+                UIManager.Instance.SetInfoTextBar(afterBoneLine);
+                return;
+            }
 
-        UIManager.Instance.SetInfoTextBar(getItemLine);
-        this.item.SetActive(true);
-        isItemGiven = true;
+            UIManager.Instance.SetInfoTextBar(getBoneLine);
+            Instantiate(bone, bonePosition.position, bonePosition.rotation);
+            isBoneGiven = true;
+        }
+        else
+        {
+            if (isItemGiven)
+            {
+                UIManager.Instance.SetInfoTextBar(afterItemLine);
+                return;
+            }
+
+            UIManager.Instance.SetInfoTextBar(getItemLine);
+            this.item.SetActive(true);
+            isItemGiven = true;
+        }
     }
 }
